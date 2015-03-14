0eb52310bd08a759e1debbf993fffd57907552d7
kuno <kuno.the.developer@gmail.com>
2015-03-14 17:58:02 +0800

    Upgrade popcorn-time.app to v0.3.7.2

--- Casks/popcorn-time.rb
+++ Casks/popcorn-time.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'popcorn-time' do
-  version '0.3.7.1'
-  sha256 'ef21b6db873ee173883390bfc83204aa28b869ee24545cc0b6706152f46710ed'
+  version '0.3.7.2'
+  sha256 'a35b5402c84b69bf54c86c278c62d93d933506966c8c819e56f97877e4d2cfb7'
 
   url "http://popcorn.obsidian.goender.net/build/Popcorn-Time-#{version.gsub(/beta-/, '')}-Mac.dmg"
   homepage 'https://popcorntime.io'
